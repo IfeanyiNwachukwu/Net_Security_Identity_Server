5d51a873355c0b8bfaef9b3de3a145d153bcb987
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2021-10-24 13:23:08 +0100

    Added new Project AspNetSecurity.IdentityServer

---
+++ AspNetSecurity/AspNetSecurity.API/Models/AspNetSecurityContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AspNetSecurity.API.Models
+{
+    public class AspNetSecurityContext : DbContext
+    {
+        public AspNetSecurityContext(DbContextOptions<AspNetSecurityContext> options):base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
--- AspNetSecurity/AspNetSecurity.IdentityServer/Startup.cs
+++ AspNetSecurity/AspNetSecurity.IdentityServer/Startup.cs
@@ -1,14 +1,17 @@
-using AspNetSecurity.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-
-namespace AspNetSecurity.API
+namespace AspNetSecurity.IdentityServer
 {
     public class Startup
     {
@@ -22,11 +25,11 @@ namespace AspNetSecurity.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AspNetSecurityContext>(opt => opt.UseInMemoryDatabase("InMem"));
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AspNetSecurity.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AspNetSecurity.IdentityServer", Version = "v1" });
             });
         }
 
@@ -37,7 +40,7 @@ namespace AspNetSecurity.API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AspNetSecurity.API v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AspNetSecurity.IdentityServer v1"));
             }
 
             app.UseRouting();
